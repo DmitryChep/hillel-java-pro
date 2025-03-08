74a5fb3277986997a42db188d0de7f6542045fe6
Dmitry Chepurko <72142811+DmitryChep@users.noreply.github.com>
2025-03-08 10:54:45 +0200

    commit for PR (#9)

--- src/main/java/ua/ithillel/javapro/Application.java
+++ src/main/java/ua/ithillel/javapro/Application.java
@@ -1,5 +1,7 @@
 package ua.ithillel.javapro;
 
 public class Application {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        System.out.println("This is very important feature!");
+    }
 }
